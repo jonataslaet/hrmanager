f7f5a48648bc7fd2bb6dc361dfa7eae7b7b0fb6e
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-10-21 09:22:57 -0300

    Setting hr-config-server to use Private Git repository - it works only on public visibility

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,32 @@
 package com.devsuperior.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner {
+	@Value("${spring.cloud.config.server.git.username}")
+	private String username;
+
+	@Value("${spring.cloud.config.server.git.password}")
+	private String password;
+	
+	@Value("${spring.cloud.config.server.git.private-key}")
+	private String key;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
 
+	public void run(String... args) throws Exception {
+
+		System.out.println("Username = " + username);
+		System.out.println("Password = " + password);
+		System.out.println("Key = " + key);
+
+	}
 }
